0bc4c927b1b6429d7a28d27df2e6afcdb558ae9c
Ekins Kuuzie <79057299+KuuZeus@users.noreply.github.com>
2024-12-02 21:47:16 +0000

    multiple attempts

--- app/components/ui/chat/chat-avatar.tsx
+++ app/components/ui/chat/chat-avatar.tsx
@@ -13,7 +13,7 @@ export function ChatMessageAvatar() {
   }
 
   return (
-    <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-black text-white shadow">
+    <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-white text-white shadow">
       <Image
         className="rounded-md"
         src="/llama.png"
